f9c6a93a9f76e28f9dfa845f276d34e8dd099602
William Mumby <williammumby@Williams-MacBook-Pro.local>
2021-03-12 07:58:24 +0000

    first run through

---
+++ basics.sql
@@ -0,0 +1,36 @@
+-- Creating a database
+
+-- CREATE DATABASE TestData
+-- GO
+
+USE TestData
+GO
+
+-- CREATE TABLE dbo.Products
+-- (
+--     Id int PRIMARY KEY NOT NULL,
+--     Name VARCHAR(25) NOT NULL,
+--     Price MONEY NULL,
+--     Description VARCHAR(MAX) NULL
+-- )
+-- GO
+
+-- INSERT dbo.Products 
+-- (Id, Name, Price, Description) 
+-- VALUES (1, 'Light', 10.99, 'A simple small light');
+-- GO
+
+-- UPDATE dbo.Products
+--     SET Name = 'Table'
+--     WHERE Id = 2
+-- GO
+
+-- SELECT * FROM Products
+
+-- SELECT * FROM Products WHERE Name = 'Light'
+-- GO
+
+-- SELECT Id, Name FROM Products
+
+-- SELECT Id, Name, Price * 1.5 AS SalePrice FROM Products
+
---
+++ views.sql
@@ -0,0 +1,13 @@
+USE TestData
+GO
+
+
+-- ------------------ Create a view
+-- CREATE VIEW VList
+--     AS
+--     SELECT Id, Name
+-- FROM Products
+-- GO
+
+-- ------------------ Query a view
+SELECT * FROM VList
